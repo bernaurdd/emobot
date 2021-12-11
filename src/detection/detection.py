c19305917101989595b4895ff235de922f08c34e
demetere <jr1kpoh2>
2021-12-11 22:35:10 +0100

    updated detection

--- src/detection/detection.py
+++ src/detection/detection.py
@@ -46,7 +46,7 @@ class EmotionDetection:
     def start_detection(self):
         # prevents openCL usage and unnecessary logging messages
         cv2.ocl.setUseOpenCL(False)
-
+        quote = None
         # dictionary which assigns each label an emotion (alphabetical order)
         emotion_dict = {0: "anger", 1: "disgust", 2: "fear", 3: "happy", 4: "neutral", 5: "sad", 6: "surprise"}
         emotion_count = {"anger": 0, "disgust": 0, "fear": 0, "happy": 0, "neutral": 0, "sad": 0, "surprise": 0}
@@ -56,23 +56,34 @@ class EmotionDetection:
         while True:
             # Find haar cascade to draw bounding box around face
             ret, frame = cap.read()
+            width = int(cap.get(3))
+            height = int(cap.get(4))
+            font = cv2.FONT_HERSHEY_SIMPLEX
+
+
+
             facecasc = cv2.CascadeClassifier(f'{self.folder_path}/haarcascade_frontalface_default.xml')
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
             for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
+                # cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
                 roi_gray = gray[y:y + h, x:x + w]
                 cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
                 prediction = self.model.predict(cropped_img)
                 maxindex = int(np.argmax(prediction))
-                cv2.putText(frame, emotion_dict[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                # cv2.putText(frame, emotion_dict[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
                 if emotion_dict[maxindex] in emotion_count:
                     emotion_count[emotion_dict[maxindex]] += 1
+
+            if quote is not None:
+                cv2.putText(frame, f'{quote}', (width // 2, height // 2), font, 1, (0, 0, 0), 5, cv2.LINE_AA)
 
             if time.perf_counter() - start_time > 10:
                 start_time = time.perf_counter()
                 emotion = max(emotion_count, key=emotion_count.get)
+                quote = self.emobot.getAdvice(emotion)
+
                 print(self.emobot.getAdvice(emotion))
 
                 emotion_count = {"anger": 0, "disgust": 0, "fear": 0, "happy": 0, "neutral": 0, "sad": 0, "surprise": 0}
