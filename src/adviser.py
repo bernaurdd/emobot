4f45a11222f94188c0aa25d746481fb994cd5037
Bennetz <bernhard.boeckenhoff@edu.udo>
2021-12-12 01:06:09 +0100

    fixed the advice

--- src/adviser.py
+++ src/adviser.py
@@ -9,8 +9,8 @@ class Adviser():
         path2 = os.getcwd() + '/ressources/wisdom.txt'
         self.prompts = pd.read_csv(path1, sep=';', header=None)
         self.prompts.columns = ['emotion', 'prompts']
-        self.advices = pd.read_csv(path2, sep='\t', header=None)
-        self.advices.columns = ['emotion', 'advice']
+        self.advices = pd.read_csv(path2, sep=';', header=None)
+        self.advices.columns = ['emotion', 'topic', 'advice']
         self.seed = 0
 
     def getPrompt(self, emotion):
@@ -19,7 +19,8 @@ class Adviser():
 
     def getAdvice(self, emotion='neutral', topic='relationship'):
         self.seed = self.seed + 1000
-        return self.advices[self.advices['emotion'] == emotion].sample(random_state=self.seed , replace=True).iloc[0,1]
+        applicableAdvice = self.advices[(self.advices['emotion'] == emotion) & (self.advices['topic'] == topic)]
+        return applicableAdvice.sample(random_state=self.seed , replace=True).iloc[0,2]
 
     def predictTopic(self, sentence):
         tokens = set(nltk.word_tokenize(sentence))
@@ -27,7 +28,7 @@ class Adviser():
         names = []
         names.append('relationship')
         keywords.append({'wife', 'relationship', 'love', 'girlfriend'})
-        names.append('Carrer')
+        names.append('career')
         keywords.append({'boss', 'job', 'money'})
         max = 0
         pred_topic = 'idk'
